5f71ff3718313d14dc9eeb35fdcf7e5f8e8c94d3
DevBadger10 <104037142+DevBadger10@users.noreply.github.com>
2022-09-04 13:15:39 +1000

    Server connection making thingy

--- Server/Main.gd
+++ Server/Main.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var hosting = false
+#var Log = ""
+#var log_file = File.new()
 
 func _ready():
 	pass # Replace with function body.
@@ -16,19 +18,25 @@ func _on_StartHosting_pressed():
 		starthosting()
 
 func starthosting():
-	print(IP.get_local_addresses())
+	#Log_String("Started hosting")
+	#print(IP.get_local_addresses())
 	var peer = NetworkedMultiplayerENet.new()
 	if $LimitPlayers.pressed == true:
 		peer.create_server(5113, 14) #14 colours.
 	if $LimitPlayers.pressed == false:
-		peer.create_server(5113, 100)
+		peer.create_server(5113, 100) # 5113 = SLIE as no character is identical to M in numbers. 
 	get_tree().network_peer = peer
 	$ConnectionID.text = "SlimeID: " + encodeIP()
 	#print(encodeIP())
 
 func stophosting():
+	#Log_String("Ended hosting")
 	get_tree().network_peer = null
 	$ConnectionID.text = "SlimeID: Terminated"
+#	if $Log.pressed:
+#		log_file.open("user://Log " + str(rand_range(0,9999)), File.WRITE)
+#		log_file.store_string(Log)
+#		log_file.close()
 
 func _on_StopHosting_pressed():
 	if hosting == true:
@@ -44,3 +52,7 @@ func encodeIP():
 	for i in IP.get_local_addresses().size():
 		if IP.get_local_addresses()[i].left(2) == "10":
 			return IP.get_local_addresses()[i]
+
+#func Log_String(string):
+#	Log = Log + str(string + "\n")
+#	print(Log)
